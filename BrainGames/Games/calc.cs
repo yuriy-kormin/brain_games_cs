b22e1abbaf3e32886cc7fe13fce4854cf5309b25
yuriy.kormin <yuriy.kormin@gmail.com>
2024-11-28 21:03:00 +0400

    update calc

--- BrainGames/Games/calc.cs
+++ BrainGames/Games/calc.cs
@@ -2,23 +2,36 @@ namespace BrainGames.CLI.Engine;
 
 public class Calc:IGame
 {
-    public string Description => "calc";
+    public string Description => "Calc expression";
 
     private int Num1 { get; set; }
     private int Num2 { get; set; }
+    private char Op { get; set; }
+    
+    private int Answer { get; set; }
 
     public string GetQuestion()
     {
+        var Operations = new Dictionary<char,Func<int,int,int>>()
+        {
+            { '+', (a,b) => a+b },
+            { '-', (a,b) => a-b },
+            { '*', (a,b) => a*b },
+            { '/', (a,b) => a/b },
+        };
+        
         Random random = new Random();
         Num1 = random.Next(1, 10);
         Num2 = random.Next(1, 10);
+        Op = Operations.Keys.ElementAt(random.Next(Operations.Count));
+        Answer = Operations[Op](Num1, Num2);
         
-        return $"What is a summ of {Num1} + {Num2}?";
+        return $"What is result of expression?{Num1} {Op} {Num2} ";
     }
 
     public bool ValidateAnswer(string answer)
     {
         int.TryParse(answer, out int result);
-        return result == (Num1+Num2);
+        return result == Answer;
     }
 }
