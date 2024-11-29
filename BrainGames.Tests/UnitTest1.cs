da7b5c3204f17da7617119bc7a910be19d69a07e
yuriy.kormin <yuriy.kormin@gmail.com>
2024-11-29 11:00:04 +0400

    add calc tests

--- BrainGames.Tests/UnitTest1.cs
+++ BrainGames.Tests/UnitTest1.cs
@@ -1,4 +1,7 @@
+using BrainGames.CLI.Engine;
+using System.Data;
 namespace BrainGames.Tests;
+using System.Reflection;
 
 public class Tests
 {
@@ -8,8 +11,24 @@ public class Tests
     }
 
     [Test]
-    public void Test1()
+    public void TestCalc()
     {
-        Assert.Pass();
+        var calc = new Calc();
+        var question = calc.GetQuestion();
+        var text = question.Split(new char[]{'?'},2);
+        StringAssert.StartsWith("What is result of expression", text[0]);
+
+        var answer = new DataTable().Compute(text[1], null);
+        Assert.IsTrue(calc.ValidateAnswer(answer.ToString()));
+    }
+
+    [Test]
+    public void TestValidateCalc()
+    {
+        var calc = new Calc();
+        var propertyInfo = typeof(Calc).GetProperty("Answer", BindingFlags.NonPublic | BindingFlags.Instance);
+        
+        propertyInfo.SetValue(calc, 12);
+        Assert.IsTrue(calc.ValidateAnswer("12"));
     }
 }
